316bb05b54881cdcf0426e0ae21597baf3f6e60c
CrazyDeveloper <acrazydeveloper@gmail.com>
2016-03-30 11:33:36 +0800

    First Commit

---
+++ src/main/java/com/crazy/developer/combatweather/MainActivity.java
@@ -0,0 +1,15 @@
+package com.crazy.developer.combatweather;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+//https://github.com/LoveJavaLonelyLife/CombatWeather.git
+}
